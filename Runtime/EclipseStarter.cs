1e3684ca0baf154153170968f1767178780611f5
MrGaFor <fedan.andriy@gmail.com>
2025-06-22 18:36:40 +0300

    Add Starter

---
+++ Runtime/EclipseStarter.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace EC
+{
+    public class EclipseStarter
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void InitAfterScene()
+        {
+            Application.runInBackground = true;
+            Application.targetFrameRate = 60;
+            PrimeTween.PrimeTweenConfig.warnEndValueEqualsCurrent = false;
+            PrimeTween.PrimeTweenConfig.warnZeroDuration = false;
+            PrimeTween.PrimeTweenConfig.warnTweenOnDisabledTarget = false;
+            PrimeTween.PrimeTweenConfig.warnStructBoxingAllocationInCoroutine = false;
+            Updater.UpdaterCore.Init();
+            Coroutine.Coroutines.Init();
+            GPU.GPUInstance.Init();
+            Settings.SettingsSystem.Init();
+            Input.InputSystem.Init();
+        }
+    }
+}
