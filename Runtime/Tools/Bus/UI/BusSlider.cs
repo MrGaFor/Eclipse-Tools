fc6feb20db86cd09b2c7aa23aeaae5931351fe8a
MrGaFor <fedan.andriy@gmail.com>
2025-06-30 18:27:04 +0300

    Bus slider update

--- Runtime/Tools/Bus/UI/BusSlider.cs
+++ Runtime/Tools/Bus/UI/BusSlider.cs
@@ -8,7 +8,9 @@ namespace EC.UI
     [HideMonoScript]
     public class BusSlider : MonoBehaviour
     {
-        [SerializeField] private string _key;
+        [SerializeField, HorizontalGroup("params", 0.5f), LabelWidth(30)] private string _key;
+        [SerializeField, HorizontalGroup("params"), LabelWidth(30)] private float _min = 0f;
+        [SerializeField, HorizontalGroup("params"), LabelWidth(30)] private float _max = 1f;
         [SerializeField, FoldoutGroup("Events")] private UnityEvent<float> _onChange;
 
         private void OnEnable()
@@ -21,14 +23,17 @@ namespace EC.UI
             BusSystem.Unsubscribe<float>(_key, OnChangeValue);
         }
 
-        public void SetValue(float state)
+        public void SetValue(float value)
         {
-            BusSystem.Set<float>(_key, state);
+            BusSystem.Set<float>(_key, Mathf.Clamp(value, _min, _max));
         }
-
+        public void SetMoveValue(float delta)
+        {
+            BusSystem.Set<float>(_key, Mathf.Clamp(BusSystem.Get<float>(_key) + delta, _min, _max));
+        }
         private void OnChangeValue(float value)
         {
-            _onChange?.Invoke(value);
+            _onChange?.Invoke(Mathf.Clamp(value, _min, _max));
         }
 
     }
