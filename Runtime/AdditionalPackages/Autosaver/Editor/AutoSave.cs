8af351a447f0812aefc6ab7e6aeccab30a4231a5
MrGaFor <fedan.andriy@gmail.com>
2025-07-04 04:00:24 +0300

    Autosaver fix

--- Runtime/AdditionalPackages/Autosaver/Editor/AutoSave.cs
+++ Runtime/AdditionalPackages/Autosaver/Editor/AutoSave.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using System.Collections;
 using UnityEditor;
 using System.Timers;
@@ -193,3 +194,4 @@ public class AutoSave : EditorWindow
 		}
 	}	
 }
+#endif
