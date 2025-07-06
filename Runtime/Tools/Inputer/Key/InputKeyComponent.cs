cd198a0247732e9105143772a97d1e4d6d02728a
MrGaFor <fedan.andriy@gmail.com>
2025-07-06 18:58:14 +0300

    Input Key fix unsubscribe

--- Runtime/Tools/Inputer/Key/InputKeyComponent.cs
+++ Runtime/Tools/Inputer/Key/InputKeyComponent.cs
@@ -10,20 +10,35 @@ namespace EC.Inputer
         [SerializeField] private KeyCode _key;
         [SerializeField, HorizontalGroup("evs")] private UnityEvent _onDown;
         [SerializeField, HorizontalGroup("evs")] private UnityEvent _onUp;
+        [SerializeField, HorizontalGroup("evs")] private UnityEvent _onClick;
+
+        private bool _isDown;
 
         private void OnEnable()
         {
             InputKeyController.Subscribe(_key, OnDown, true);
             InputKeyController.Subscribe(_key, OnUp, false);
         }
+        private void OnDisable()
+        {
+            InputKeyController.Unsubscribe(_key, OnDown, true);
+            InputKeyController.Unsubscribe(_key, OnUp, false);
+        }
 
-        public void OnDown() 
+        public void OnDown()
         {
             _onDown?.Invoke();
+            _isDown = true;
         }
         public void OnUp()
         {
             _onUp?.Invoke();
+            if (_isDown)
+            {
+                _isDown = false;
+                OnClick();
+            }
         }
+        public void OnClick() => _onClick?.Invoke();
     }
 }
