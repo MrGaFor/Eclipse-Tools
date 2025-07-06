14699fd9b580f76f9e6e7a727fd719f638fccdbd
MrGaFor <fedan.andriy@gmail.com>
2025-07-06 19:14:02 +0300

    Input key Limit call count

--- Runtime/Tools/Inputer/Key/InputKeyComponent.cs
+++ Runtime/Tools/Inputer/Key/InputKeyComponent.cs
@@ -10,13 +10,15 @@ namespace EC.Inputer
         private enum CallCount { Any, Limit }
         [SerializeField, HorizontalGroup("sett"), LabelWidth(60)] private KeyCode _key;
         [SerializeField, HorizontalGroup("sett", 140), LabelWidth(60), LabelText("Count")] private CallCount _callCount;
-        [SerializeField, HorizontalGroup("sett", 80), HideLabel, ShowIf("_callCount", CallCount.Limit)] private int _limitCount;
+        [SerializeField, HorizontalGroup("sett", 80), HideLabel, ShowIf("_callCount", CallCount.Limit), Min(1)] private int _limitCount = 1;
         [SerializeField, FoldoutGroup("Events"), HorizontalGroup("Events/evs")] private UnityEvent _onDown;
         [SerializeField, FoldoutGroup("Events"), HorizontalGroup("Events/evs")] private UnityEvent _onUp;
 
+        private int _leftCount;
 
         private void OnEnable()
         {
+            if (_callCount == CallCount.Limit) _leftCount = _limitCount;
             InputKeyController.Subscribe(_key, OnDown, true);
             InputKeyController.Subscribe(_key, OnUp, false);
         }
@@ -28,10 +30,13 @@ namespace EC.Inputer
 
         public void OnDown()
         {
+            if (_callCount == CallCount.Limit && _leftCount == 0) return;
+            if (_callCount == CallCount.Limit) _leftCount--;
             _onDown?.Invoke();
         }
         public void OnUp()
         {
+            if (_callCount == CallCount.Limit && _leftCount == 0) return;
             _onUp?.Invoke();
         }
     }
