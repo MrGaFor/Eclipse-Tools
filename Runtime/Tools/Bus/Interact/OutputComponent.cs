238146042d9e6cfeed8750ca9e097d9d1a62bec5
MrGaFor <fedan.andriy@gmail.com>
2025-10-12 09:37:50 +0300

    BusOutput Added Handle calling

--- Runtime/Tools/Bus/Interact/OutputComponent.cs
+++ Runtime/Tools/Bus/Interact/OutputComponent.cs
@@ -19,7 +19,7 @@ namespace EC.Bus
         private void OnEnable() { BusSystem.Subscribe(_key, OnInvoke); if (_autoCallType == AutoCall.OnEnable) OnInvoke(); }
         private void OnDisable() { BusSystem.Unsubscribe(_key, OnInvoke); if (_autoCallType == AutoCall.OnDisable) OnInvoke(); }
         private void OnDestroy() { if (_autoCallType == AutoCall.OnDestroy) OnInvoke(); }
-        private void OnInvoke() => _event?.Invoke();
+        public void OnInvoke() => _event?.Invoke();
     }
     [HideMonoScript]
     public class OutputComponent<T> : MonoBehaviour
@@ -31,11 +31,12 @@ namespace EC.Bus
 
         [SerializeField] protected UnityEvent<T> _event;
 
-        private void Awake() { if (_autoCallType == AutoCall.Awake && BusSystem.HasKey(_key)) OnInvoke(BusSystem.Get<T>(_key)); }
-        private void Start() { if (_autoCallType == AutoCall.Start && BusSystem.HasKey(_key)) OnInvoke(BusSystem.Get<T>(_key)); }
-        private void OnEnable() { BusSystem.Subscribe<T>(_key, OnInvoke); if (_autoCallType == AutoCall.OnEnable && BusSystem.HasKey(_key)) OnInvoke(BusSystem.Get<T>(_key)); }
-        private void OnDisable() { BusSystem.Unsubscribe<T>(_key, OnInvoke); if (_autoCallType == AutoCall.OnDisable && BusSystem.HasKey(_key)) OnInvoke(BusSystem.Get<T>(_key)); }
-        private void OnDestroy() { if (_autoCallType == AutoCall.OnDestroy && BusSystem.HasKey(_key)) OnInvoke(BusSystem.Get<T>(_key)); }
-        private void OnInvoke(T value) => _event?.Invoke(value);
+        private void Awake() { if (_autoCallType == AutoCall.Awake) OnInvoke(); }
+        private void Start() { if (_autoCallType == AutoCall.Start) OnInvoke(); }
+        private void OnEnable() { BusSystem.Subscribe<T>(_key, OnInvoke); if (_autoCallType == AutoCall.OnEnable) OnInvoke(); }
+        private void OnDisable() { BusSystem.Unsubscribe<T>(_key, OnInvoke); if (_autoCallType == AutoCall.OnDisable) OnInvoke(); }
+        private void OnDestroy() { if (_autoCallType == AutoCall.OnDestroy) OnInvoke(); }
+        public void OnInvoke(T value) => _event?.Invoke(value);
+        public void OnInvoke() { if (BusSystem.HasKey(_key)) OnInvoke(BusSystem.Get<T>(_key)); }
     }
 }
