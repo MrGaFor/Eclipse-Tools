5e5cfad92c64948928b59225f7a18a83727b4a1b
MrGafor <108179226+MrGaFor@users.noreply.github.com>
2026-03-18 11:49:07 +0200

    Locator Fix

--- Runtime/Tools/Locator/GameLocator.cs
+++ Runtime/Tools/Locator/GameLocator.cs
@@ -8,47 +8,55 @@ namespace EC.Services
     public class GameLocator
     {
         private static GameLocator _instance;
-        private readonly Dictionary<Type, object> _services = new();
-        private readonly Dictionary<Type, List<Action<object>>> _waiters = new();
+
+        private readonly Dictionary<Type, GameService> _services = new();
+        private readonly Dictionary<Type, List<Action<GameService>>> _waiters = new();
 
         private static GameLocator Instance => _instance ??= new GameLocator();
 
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
         private static void PreInit()
         {
             _instance = null;
         }
 
         #region Registration
-        public static async void Register<T>(T service) where T : GameService => await RegisterAsync(service);
-        public static async UniTask RegisterAsync<T>(T service) where T : GameService
+        public static UniTask Register<T>(T service) where T : GameService
         {
-            if (Instance._services.ContainsKey(typeof(T))) return;
-            await ForceRegisterAsync(service);
+            return RegisterInternal(service, false);
         }
 
-        public static async void ForceRegister<T>(T service) where T : GameService => await ForceRegisterAsync(service);
-        public static async UniTask ForceRegisterAsync<T>(T service) where T : GameService
+        public static UniTask ForceRegister<T>(T service) where T : GameService
         {
-            Instance._services[typeof(T)] = service;
+            return RegisterInternal(service, true);
+        }
+
+        private static async UniTask RegisterInternal(GameService service, bool force)
+        {
+            var type = service.GetType();
+            if (!force && Instance._services.ContainsKey(type))
+                return;
+            Instance._services[type] = service;
             service.OnCreate();
             await service.OnCreateAsync();
-            if (Instance._waiters.TryGetValue(typeof(T), out var list))
+            if (Instance._waiters.TryGetValue(type, out var list))
             {
-                foreach (var cb in list) cb(service);
-                Instance._waiters.Remove(typeof(T));
+                foreach (var cb in list)
+                    cb(service);
+                Instance._waiters.Remove(type);
             }
         }
 
-        public static async void Unregister<T>() where T : GameService => await UnregisterAsync<T>();
-        public static async UniTask UnregisterAsync<T>() where T : GameService
+        public static UniTask Unregister<T>() where T : GameService
         {
-            if (Instance._services.TryGetValue(typeof(T), out var service))
+            var type = typeof(T);
+            if (Instance._services.TryGetValue(type, out var service))
             {
-                ((T)service).OnDispose();
-                await ((T)service).OnDisposeAsync();
-                Instance._services.Remove(typeof(T));
+                Instance._services.Remove(type);
+                service.OnDispose();
+                return service.OnDisposeAsync();
             }
+            return UniTask.CompletedTask;
         }
         #endregion
 
@@ -65,25 +73,33 @@ namespace EC.Services
                 service = (T)s;
                 return true;
             }
-            service = default;
+            service = null;
             return false;
         }
 
-        public static async UniTask<(bool success, T service)> TryGetAsync<T>(float timeoutSeconds) where T : GameService
+        public static async UniTask<(bool success, T service)> TryGetAsync<T>(float timeoutSeconds = 1)
+            where T : GameService
         {
-            if (Instance._services.TryGetValue(typeof(T), out var existing))
+            var type = typeof(T);
+            if (Instance._services.TryGetValue(type, out var existing))
                 return (true, (T)existing);
             var tcs = new UniTaskCompletionSource<(bool, T)>();
-            void callback(object s) => tcs.TrySetResult((true, (T)s));
-            if (!Instance._waiters.TryGetValue(typeof(T), out var list))
-                Instance._waiters[typeof(T)] = list = new List<Action<object>>();
-            list.Add(callback);
+            void Callback(GameService s)
+            {
+                tcs.TrySetResult((true, (T)s));
+            }
+            if (!Instance._waiters.TryGetValue(type, out var list))
+            {
+                list = new List<Action<GameService>>();
+                Instance._waiters[type] = list;
+            }
+            list.Add(Callback);
             var delayTask = UniTask.Delay(TimeSpan.FromSeconds(timeoutSeconds));
-            var (firstCompleted, result) = await UniTask.WhenAny(tcs.Task, delayTask);
-            if (firstCompleted)
+            var (completedIndex, result) = await UniTask.WhenAny(tcs.Task, delayTask);
+            if (completedIndex)
                 return result;
-            list.Remove(callback);
-            return (false, default);
+            list.Remove(Callback);
+            return (false, null);
         }
         #endregion
     }
--- Runtime/Tools/Locator/SceneLocator.cs
+++ Runtime/Tools/Locator/SceneLocator.cs
@@ -9,8 +9,8 @@ namespace EC.Services
     public class SceneLocator : MonoBehaviour
     {
         private static SceneLocator _instance;
-        private readonly Dictionary<Type, object> _services = new();
-        private readonly Dictionary<Type, List<Action<object>>> _waiters = new();
+        private readonly Dictionary<Type, SceneService> _services = new();
+        private readonly Dictionary<Type, List<Action<SceneService>>> _waiters = new();
 
         private static SceneLocator Instance
         {
@@ -31,45 +31,53 @@ namespace EC.Services
         }
         private void OnDestroy()
         {
-            if (Instance == this)
-                foreach (var service in Instance._services)
-                    if (service.Value is SceneService ss)
-                    {
-                        ss.OnDispose();
-                        ss.OnDisposeAsync().Forget();
-                    }
+            if (_instance != this) return;
+            foreach (var service in _services.Values)
+            {
+                service.OnDispose();
+                service.OnDisposeAsync().Forget();
+            }
         }
 
         #region Registration
-        public static async void Register<T>(T service) where T : SceneService => await RegisterAsync(service);
-        public static async UniTask RegisterAsync<T>(T service) where T : SceneService
+        public static UniTask Register<T>(T service) where T : SceneService
         {
-            if (Instance._services.ContainsKey(typeof(T))) return;
-            await ForceRegisterAsync(service);
+            return RegisterInternal(service, false);
         }
 
-        public static async void ForceRegister<T>(T service) where T : SceneService => await ForceRegisterAsync(service);
-        public static async UniTask ForceRegisterAsync<T>(T service) where T : SceneService
+        public static UniTask ForceRegister<T>(T service) where T : SceneService
         {
-            Instance._services[typeof(T)] = service;
+            return RegisterInternal(service, true);
+        }
+        private static async UniTask RegisterInternal(SceneService service, bool force)
+        {
+            var type = service.GetType();
+            if (!force && Instance._services.ContainsKey(type))
+                return;
+            Instance._services[type] = service;
             service.OnCreate();
             await service.OnCreateAsync();
-            if (Instance._waiters.TryGetValue(typeof(T), out var list))
+            if (Instance._waiters.TryGetValue(type, out var list))
             {
-                foreach (var cb in list) cb(service);
-                Instance._waiters.Remove(typeof(T));
+                foreach (var cb in list)
+                    cb(service);
+
+                Instance._waiters.Remove(type);
             }
         }
 
-        public static async void Unregister<T>() where T : SceneService => await UnregisterAsync<T>();
-        public static async UniTask UnregisterAsync<T>() where T : SceneService
+        public static UniTask Unregister<T>() where T : SceneService
         {
-            if (Instance._services.TryGetValue(typeof(T), out var service))
+            var type = typeof(T);
+
+            if (Instance._services.TryGetValue(type, out var service))
             {
-                ((T)service).OnDispose();
-                await ((T)service).OnDisposeAsync();
-                Instance._services.Remove(typeof(T));
+                Instance._services.Remove(type);
+                service.OnDispose();
+                return service.OnDisposeAsync();
             }
+
+            return UniTask.CompletedTask;
         }
         #endregion
 
@@ -86,25 +94,33 @@ namespace EC.Services
                 service = (T)s;
                 return true;
             }
-            service = default;
+            service = null;
             return false;
         }
 
-        public static async UniTask<(bool success, T service)> TryGetAsync<T>(float timeoutSeconds) where T : SceneService
+        public static async UniTask<(bool success, T service)> TryGetAsync<T>(float timeoutSeconds = 1)
+            where T : SceneService
         {
-            if (Instance._services.TryGetValue(typeof(T), out var existing))
+            var type = typeof(T);
+            if (Instance._services.TryGetValue(type, out var existing))
                 return (true, (T)existing);
             var tcs = new UniTaskCompletionSource<(bool, T)>();
-            void callback(object s) => tcs.TrySetResult((true, (T)s));
-            if (!Instance._waiters.TryGetValue(typeof(T), out var list))
-                Instance._waiters[typeof(T)] = list = new List<Action<object>>();
-            list.Add(callback);
+            void Callback(SceneService s)
+            {
+                tcs.TrySetResult((true, (T)s));
+            }
+            if (!Instance._waiters.TryGetValue(type, out var list))
+            {
+                list = new List<Action<SceneService>>();
+                Instance._waiters[type] = list;
+            }
+            list.Add(Callback);
             var delayTask = UniTask.Delay(TimeSpan.FromSeconds(timeoutSeconds));
             var (firstCompleted, result) = await UniTask.WhenAny(tcs.Task, delayTask);
             if (firstCompleted)
                 return result;
-            list.Remove(callback);
-            return (false, default);
+            list.Remove(Callback);
+            return (false, null);
         }
         #endregion
     }
